8b8c8ac0daae651ee0be8bc1ffb316bb1ec4e57f
Tito <titusmunene100@gmail.com>
2025-06-16 16:45:28 +0300

    styling my todo app

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -1,9 +1,12 @@
+import Header from "./Components/Header";
 import Todo from "./Components/Todo";
+import"./App.css"
 
 function App() {
   return (
     <div className="App">
-     <Todo/>
+      <Header />
+      <Todo />
     </div>
   );
 }
