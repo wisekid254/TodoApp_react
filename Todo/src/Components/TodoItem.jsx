b7831c200b5d7689ba3a07880d39639e9976f6c1
Tito <titusmunene100@gmail.com>
2025-06-17 16:58:09 +0300

    finished all the delete functionality

--- Todo/src/Components/TodoItem.jsx
+++ Todo/src/Components/TodoItem.jsx
@@ -1,6 +1,25 @@
-export default function TodoItem({item}) {
+import styles from "./todoitem.module.css";
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo)=>todo!==item));
+    
+  }
+
   return (
-     <h3>{item}</h3>
-  )
- 
-};
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
+      <hr className={styles.line} />
+    </div>
+  );
+}
