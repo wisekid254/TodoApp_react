fd62d1b7fcc495c900eca58ea8513ce40b0b932f
Tito <titusmunene100@gmail.com>
2025-06-18 17:28:00 +0300

    Marking items as completed

--- Todo/src/Components/TodoItem.jsx
+++ Todo/src/Components/TodoItem.jsx
@@ -1,14 +1,21 @@
 import styles from "./todoitem.module.css";
 export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
-    setTodos(todos.filter((todo)=>todo!==item));
-    
+    setTodos(todos.filter((todo) => todo !== item));
   }
+  function handleClick(name) {
+    const newArray = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    )
+    setTodos(newArray)
+   
+  }
+  const styleName = item.done ? styles.completed : ""
 
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}
+        <span className={styleName} onClick={() => handleClick(item.name)}>{item.name}</span>
         <span>
           <button
             onClick={() => handleDelete(item)}
--- Todo/src/Components/TodoList.jsx
+++ Todo/src/Components/TodoList.jsx
@@ -1,10 +1,15 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
-export default function TodoList({ todos,setTodos }) {
+export default function TodoList({ todos, setTodos }) {
   return (
     <div className={styles.list}>
       {todos.map((item) => (
-        <TodoItem key={item} item={item} todos={todos} setTodos={setTodos} />
+        <TodoItem
+          key={item.name}
+          item={item}
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </div>
   );
